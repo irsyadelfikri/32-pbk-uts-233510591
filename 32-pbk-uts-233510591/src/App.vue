d5d8de0459610630fd5c25f3c40f4f3331eef6f8
irsyadel fikri <irsyadelfikri7@gmail.com>
2025-04-26 21:54:01 +0700

    Menampilkan judul aplikasi

--- 32-pbk-uts-233510591/src/App.vue
+++ 32-pbk-uts-233510591/src/App.vue
@@ -3,15 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<main class="container">
+    <h1>Daftar Kegiatan</h1>
+  </main>
 </template>
 
 <style scoped>
